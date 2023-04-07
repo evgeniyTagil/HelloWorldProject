6872405188826ed698e0453473806c7baa54656d
evgeniyTagil <evgeniytagil.road@gmail.com>
2023-04-07 22:50:29 +0300

    lesson 6, home work ex1

---
+++ src/lesson6/homework/MainClass.java
@@ -0,0 +1,10 @@
+package lesson6.homework;
+
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        CredicCard credicCard = new CredicCard();
+        credicCard.authorization();
+    }
+}
