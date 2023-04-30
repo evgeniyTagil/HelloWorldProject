409b4ec1628234a75a9bd4788746e9f28dc762eb
evgeniyTagil <evgeniytagil.road@gmail.com>
2023-04-30 21:46:11 +0300

    lesson 11, home works ex1

--- src/lesson10/homework/ExerciseOne.java
+++ src/lesson10/homework/ExerciseOne.java
@@ -2,20 +2,14 @@ package lesson10.homework;
 
 import java.util.Scanner;
 
-public class ExerciseOne{
-    public static void main(String[] args) {
+public class ExerciseOne {
+    public static void main(String[] args) throws CharacterSequenceException {
         String string = "1111-AAA-1111-AAA-1A1A";
-        /*
-        Сделал доступ реализацию доступа специально разную
-         */
         MethodToImplement method = new MethodToImplement();
-        method.digitalOutput(string); //при обычном методе
-        MethodToImplement.replacement(string);
-        MethodToImplement.printLetters(string);
-        MethodToImplement.checkLetters(string);
-        MethodToImplement.checkNumbers(string);
-        MethodToImplement.checkEndString(string);
-
-
+        try {
+            method.checkInSymbols(string);
+        } catch (CharacterSequenceException e) {
+            throw new CharacterSequenceException("Exception");
+        }
     }
 }
--- src/lesson10/homework/MethodToImplement.java
+++ src/lesson10/homework/MethodToImplement.java
@@ -2,30 +2,48 @@ package lesson10.homework;
 
 public class MethodToImplement {
 
-    public void digitalOutput(String string) {
+    public void digitalOutput(String string) throws CharacterSequenceException {
         String firstBlock = string.substring(0, 4);
         String twoBlock = string.substring(9, 13);
         System.out.println(firstBlock + " " + twoBlock);
     }
 
-    public static void replacement(String string) {
+    public void replacement(String string) throws CharacterSequenceException {
         System.out.println(string.replace("AAA", "***"));
     }
 
-    public static void printLetters(String string) {
+    public void printLetters(String string) throws CharacterSequenceException {
         System.out.println(string.substring(5, 8) + "/" + string.substring(14, 17) +
                 "/" + string.substring(19, 20) + "/" + string.substring(21, 22));
     }
 
-    public static void checkLetters(String string) {
+    public void printInStringBuilder(String string) throws CharacterSequenceException {
+        String result = string.toUpperCase();
+        String[] arr = result.split("-");
+        StringBuilder builder = new StringBuilder("Letters: ");
+        builder.append(arr[1]).append(arr[3]).append(arr[4].charAt(1)).append(arr[4].charAt(3));
+        String row = arr[1] + "/" + arr[3] + "/" + arr[4].charAt(1) + "/" + arr[4].charAt(3);
+        System.out.println(row);
+    }
+
+    public void checkLetters(String string) throws CharacterSequenceException {
         System.out.println(string.endsWith("adc") & (string.endsWith("ABC")));
     }
 
-    public static void checkNumbers(String string) {
+    public void checkNumbers(String string) throws CharacterSequenceException {
         System.out.println(string.startsWith("555"));
     }
 
-    public static void checkEndString(String string) {
+    public void checkEndString(String string) throws CharacterSequenceException {
         System.out.println(string.endsWith("1a2b"));
+    }
+
+    public void checkInSymbols(String string) throws CharacterSequenceException {
+        String sample = "ABC";
+        if (string.substring(5, 8).equals(sample)) {
+            System.out.println("Совпало");
+        } else {
+            System.out.println("Не совпало");
+        }
     }
 }
