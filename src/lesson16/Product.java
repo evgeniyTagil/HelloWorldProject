deb3ee302ab77872ac8139f00a03ae13087f91f0
evgeniyTagil <evgeniytagil.road@gmail.com>
2023-05-23 20:33:02 +0300

    lesson 16, class work

---
+++ src/lesson16/Product.java
@@ -0,0 +1,42 @@
+package lesson16;
+
+public class Product {
+    private String productName;
+    private int price;
+
+    public Product(String productName, int price) {
+        this.productName = productName;
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Product product = (Product) o;
+
+        return productName.equals(product.productName);
+    }
+
+    @Override
+    public int hashCode() {
+        return productName.hashCode();
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + productName + '\'' +
+                ", price=" + price +
+                '}';
+    }
+}
---
+++ src/lesson16/User.java
@@ -0,0 +1,27 @@
+package lesson16;
+
+public class User {
+    int age;
+    String name;
+
+    public User(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
