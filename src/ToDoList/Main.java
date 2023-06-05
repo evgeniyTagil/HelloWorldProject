c036492a221dddd5bb771c2b17b7d09a9fbdc500
evgeniyTagil <evgeniytagil.road@gmail.com>
2023-06-05 21:51:03 +0300

    lesson 20, classwork

---
+++ src/ToDoList/Main.java
@@ -0,0 +1,13 @@
+package ToDoList;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //Methods.menu();
+        File file = new File("C:\\Users\\user\\IdeaProjects\\HelloWorldProject\\src\\ToDoList\\ToDo.json");
+        //ToDoData.getAllTask(file,ToDo.);
+
+    }
+}
