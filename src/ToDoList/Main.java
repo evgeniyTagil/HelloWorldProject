357c61b20fa71e7d56bc9d5cf97f1e413130d9c7
evgeniyTagil <evgeniytagil.road@gmail.com>
2023-06-17 18:49:09 +0300

    ToDo

--- src/ToDoList/Main.java
+++ src/ToDoList/Main.java
@@ -1,13 +1,9 @@
 package ToDoList;
 
 import java.io.File;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Methods.menu();
-        File file = new File("C:\\Users\\user\\IdeaProjects\\HelloWorldProject\\src\\ToDoList\\ToDo.json");
-        //ToDoData.getAllTask(file,ToDo.);
-
+        MenuToDo.menu();
     }
 }
